81362f18c96add2d4fae7a9df3eaa0797b86aff8
brenetic <b@brenetic.com>
2020-03-04 17:23:52 +0000

    Change simplecov output format
    Recent changes to simplecov now try to require a file that does not
    exist. See https://github.com/chetan/simplecov-console/compare/v0.7.0...v0.7.1
    
    Changing the output to block bypasses this.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -101,7 +101,7 @@ SimpleCov.start do
   add_filter '/spec/'
 end
 SimpleCov.minimum_coverage 98
-
+SimpleCov::Formatter::Console.output_style = 'block'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   SimpleCov::Formatter::Console
