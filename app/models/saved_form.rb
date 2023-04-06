809eee6d199833d9c84559d6a6a5d40d5a0b4d2f
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-04-06 17:08:35 +0100

    Add db sweeper extension and specs

--- app/models/saved_form.rb
+++ app/models/saved_form.rb
@@ -1,5 +1,27 @@
 class SavedForm < ApplicationRecord
   self.implicit_order_column = 'created_at'
   validates :service_slug, presence: true
-  validates :user_id, presence: true
+
+  def invalidated?
+    return true if self.active == false
+    return true if self.attempts > 3
+    return true if self.user_id.blank? || self.user_token.blank?
+    false
+  end
+
+  def invalidate_user_fields!
+    self.user_id = ""
+    self.user_token = ""
+    self.user_data_payload = ""
+    self.secret_answer = ""
+    self.active = false
+
+    self.save!
+  end
+
+  def increment_attempts!
+    self.attempts += 1
+
+    self.save!
+  end
 end
--- spec/models/saved_form_spec.rb
+++ spec/models/saved_form_spec.rb
@@ -2,10 +2,63 @@ require 'rails_helper'
 
 RSpec.describe SavedForm, type: :model do
   it { should validate_presence_of(:service_slug) }
-  it { should validate_presence_of(:user_id) }
 
   it 'should set the default values' do
     expect(subject.attempts).to eq(0)
     expect(subject.active).to eq(true)
+  end
+
+  context '#invalidated' do
+    let(:model) { SavedForm.new(user_id: '123', user_token: '456', user_data_payload: 'q1 => answer', secret_answer: 'hello', active: true, service_slug: 'some_slug') }
+    
+    it 'should consider the model invalidated if user id is empty' do
+      expect(model.invalidated?).to be(false)
+      model.user_id = nil
+      expect(model.invalidated?).to be(true)
+      model.user_id = ""
+      expect(model.invalidated?).to be(true)
+    end
+
+    it 'should consider the model invalidated if user token is empty' do
+      expect(model.invalidated?).to be(false)
+      model.user_token = nil
+      expect(model.invalidated?).to be(true)
+      model.user_token = ""
+      expect(model.invalidated?).to be(true)
+    end
+
+    it 'should consider the model invalidated if attempts >3' do
+      expect(model.invalidated?).to be(false)
+      model.increment_attempts!
+      expect(model.invalidated?).to be(false)
+      model.increment_attempts!
+      expect(model.invalidated?).to be(false)
+      model.increment_attempts!
+      expect(model.invalidated?).to be(false)
+      model.increment_attempts!
+      expect(model.invalidated?).to be(true)
+    end
+
+    it 'should consider the model invalidated if marked inactive' do
+      expect(model.invalidated?).to be(false)
+      model.active = false
+      expect(model.invalidated?).to be(true)
+    end
+  end
+
+  context '#invalidate_user_fields' do
+    let(:model) { SavedForm.new(user_id: '123', user_token: '456', user_data_payload: 'q1 => answer', secret_answer: 'hello', active: true, service_slug: 'some_slug') }
+
+    it 'should remove sensitive data and invalidate the record' do
+      model.invalidate_user_fields!
+
+      expect(model.user_id.blank?).to be(true)
+      expect(model.user_token.blank?).to be(true)
+      expect(model.user_data_payload.blank?).to be(true)
+      expect(model.secret_answer.blank?).to be(true)
+      expect(model.active).to be(false)
+
+      expect(model.invalidated?).to be(true)
+    end
   end
 end
