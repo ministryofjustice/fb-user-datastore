d0d44c4e032f39f79c22aa9ec932ceb7b1be2d0a
StevenLeighton21 <steven.leighton@digital.justice.gov.uk>
2023-04-24 12:07:59 +0100

    Update db/migrate/20230424105345_change_saved_forms_attempts_to_integer.rb
    Co-authored-by: Nat <njseeto@users.noreply.github.com>

--- db/migrate/20230424105345_change_saved_forms_attempts_to_integer.rb
+++ db/migrate/20230424105345_change_saved_forms_attempts_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeSavedFormsAttemptsToInteger < ActiveRecord::Migration[6.1]
   def change
-    change_column :saved_forms, :attempts, :integer
+    change_column :saved_forms, :attempts, :integer, default: 0
   end
 end
